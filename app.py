0a973e071b4f17bf591a89a7cda762d6d12bce85
Randal Carr <randalrcarr@gmail.com>
2023-12-05 13:45:21 -0600

    user interface setup complete

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from tkinter import *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
-
+def generate_password():
+    pass
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -12,7 +13,33 @@ window.config(padx=20, pady=20)
 canvas = Canvas(width=200, height=200)
 logo_photo = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_photo)
-canvas.pack()
+canvas.grid(row=1, column=1)
+
+# website field setup
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=2)
+website_input = Entry(width=35)
+website_input.grid(row=2, column=1, columnspan=2)
+
+# email field setup
+email_label = Label(text="Email/Username:")
+email_label.grid(column=0, row=3)
+email_input = Entry(width=35)
+email_input.grid(column=1, row=3, columnspan=2)
+
+# password field setup
+password_label = Label(text="Password:")
+password_label.grid(column=0, row=4)
+password_input = Entry(width=18)
+password_input.grid(column=1, row=4)
+password_button = Button(text="Generate Password")
+password_button.grid(column=2, row=4)
+password_button.config(command=generate_password)
+
+# submit button field
+submit_info_button = Button(text="Add", width=36)
+submit_info_button.grid(column=1, row=5, columnspan=2)
+
 
 
 
