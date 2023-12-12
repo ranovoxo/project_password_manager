c9b0e10c188552e5237a5d48ac0f67cc4fa61b1a
Randal Carr <randalrcarr@gmail.com>
2023-12-12 16:11:34 -0600

    Added error handling and changed output file to json format

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox as mb
 from random import choice, randint, shuffle
 import pyperclip3
+import json
 
 LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 NUMBERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -22,38 +23,64 @@ def generate_password():
     shuffle(pwd_list)
     password = "".join(pwd_list)
     password_input.insert(0, password)
+    # copies password to user clipboard
     pyperclip3.copy(password)
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_to_file():
-    f = open("data.txt", "a")
     website = website_input.get()
     email = email_input.get()
     pwd = password_input.get()
-    
-    data = website + " | " + email + " | " + pwd + "\n"
 
-    result = mb.askyesno(title=website, 
-                      message=f"""Here are the details you entered: 
-                      Email: {email}
-                      Password: {pwd}
-                      Is it ok to save?""")
-    if result == True:
+    new_data = {
+        website: {
+            "email": email,
+            "password": pwd
+        }    
+    }
+    try:
+        with open("data.json", "r") as data_file:
+            # read old data
+            data = json.load(data_file)
+
+    except FileNotFoundError:
+        # create file using w and dump data
+        with open("data.json", "w") as data_file:
+            json.dump(new_data, data_file, indent=4)
+    else:
+        data.update(new_data)
+
+        with open("data.json", "w") as data_file:
+            json.dump(new_data, data_file, indent=4)
+    finally:
         website_input.delete(0, END) 
         password_input.delete(0,END)
-        f.write(data)
-        f.close()
-    elif result == False:
-        pass
 
 def check_fields():
     if len(website_input.get()) == 0:
         mb.showwarning(title="Empty Field", message="Please dont leave Website field empty")
-    elif len(email_input.get()) == 0:
-         mb.showwarning(title="Empty Field", message="Please dont leave Email field empty")
     elif(len(password_input.get()) == 0):
          mb.showwarning(title="Empty Field", message="Please dont leave Password field empty")
     else:
         save_to_file()
+
+def search_login():
+    website = website_input.get()
+
+    try:
+        with open("data.json", "r") as data_file:
+            data = json.load(data_file)
+            
+    except FileNotFoundError:
+        mb.showwarning(title="No Passwords", message="You have no passwords saved")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            mb.showinfo(title=website, message=f"Email: {email}\n Password: {password}")
+        else:
+            mb.showwarning(title="Does not Exist", message=f"Login credentials for {website} does not exist")
+            
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -72,6 +99,11 @@ website_label.grid(column=0, row=2)
 website_input = Entry(width=35)
 website_input.grid(row=2, column=1, columnspan=2)
 website_input.focus()
+
+#search field setup
+search_button = Button(text="Search", width=14)
+search_button.grid(row=2, column=3)
+search_button.config(command=search_login)
 
 # email field setup
 email_label = Label(text="Email/Username:")
